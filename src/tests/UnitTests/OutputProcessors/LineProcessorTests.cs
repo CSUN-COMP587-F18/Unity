ca988c1cce17ffe5c239650e3d339d335cc665f0
PatGarza20 <patricio.garza@yahoo.com>
2018-10-17 22:46:44 -0700

    Pull Request 4
    Added new unit tests for LineProcessor to verify that its various line processing functions work correctly. Also added new GitRemote test to check its hashcode encoding function. Finally, added comments to the tests to make life easier for reviewers.

---
+++ src/tests/UnitTests/OutputProcessors/LineProcessorTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using TestUtils;
+using NUnit.Framework;
+using GitHub.Unity;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LineProcessorTests
+    {
+        // This test verifies that ReadUntil correctly stops reading a line at the indicated char.
+        [Test]
+        public void LineShouldReadUntilChar()
+        {
+            var line = new LineParser("apple orange banana");
+            Assert.True(line.ReadUntil('b').Equals("apple orange "));
+        }
+
+        // This test verifies that ReadUntilWhitespace stops reading at the first whitespace in the line.
+        [Test]
+        public void LineShouldReadUntilWhitespace()
+        {
+            var line = new LineParser("apple orange banana");
+            Assert.True(line.ReadUntilWhitespace().Equals("apple"));
+        }
+
+        // This test verifies that ReadChunk reads exclusively between the two indicated chars.
+        [Test]
+        public void LineShouldReadChunk()
+        {
+            var line = new LineParser("apple orange banana");
+            Assert.True(line.ReadChunk('o', 'e').Equals("rang"));
+        }
+
+        // This test verifies that ReadToEnd reads until the last char in a line.
+        [Test]
+        public void LineShouldReadToEnd()
+        {
+            var line = new LineParser("apple orange banana");
+            Assert.True(line.ReadToEnd().Equals("apple orange banana"));
+        }
+
+        // This test verifies that ReadUntilLast reads until the last string indicated.
+        [Test]
+        public void LineShouldReadUntilLastString()
+        {
+            var line = new LineParser("apple orange banana");
+            Assert.True(line.ReadUntilLast("ana").Equals("apple orange ban"));
+        }
+    }
+}
+
